efeada7945344b87071cdd37b53853361cd14bee
Sarah Withee <gitlab@geekygirlsarah.com>
2021-06-01 00:56:40 -0400

    Add test files I didn't commit before

---
+++ TestPlans.py
@@ -0,0 +1,38 @@
+import unittest
+from Plans import Single_Plan, Plans
+
+
+class TestPlans(unittest.TestCase):
+
+    def test_create_single_plan(self):
+        plan_id = "123ABC456DEF"
+        state = "AA"
+        metal_level = "Brass"
+        rate = 123.45
+        rate_area = 123
+        p = Single_Plan(plan_id, state, metal_level, rate, rate_area)
+
+        self.assertEquals(p.plan_id, plan_id)
+        self.assertEquals(p.state, state)
+        self.assertEquals(p.metal_level, metal_level)
+        self.assertEquals(p.rate, rate)
+        self.assertEquals(p.rate_area, rate_area)
+
+    def test_add(self):
+        plan_id = "123ABC456DEF"
+        state = "AA"
+        metal_level = "Brass"
+        rate = 123.45
+        rate_area = 123
+
+        p = Plans()
+        p.add(plan_id, state, metal_level, rate, rate_area)
+        key = state + str(rate_area)
+
+        added_plan = p.plans[key]
+
+        self.assertEquals(added_plan.plan_id, plan_id)
+        self.assertEquals(added_plan.state, state)
+        self.assertEquals(added_plan.metal_level, metal_level)
+        self.assertEquals(added_plan.rate, rate)
+        self.assertEquals(added_plan.rate_area, rate_area)
---
+++ TestZips.py
@@ -0,0 +1,38 @@
+import unittest
+
+from Zips import Single_Zip, Zips
+
+
+class TestZips(unittest.TestCase):
+
+    def test_create_single_zip(self):
+        zipcode = 12345
+        state = "AA"
+        county_code = "54321"
+        name = "County"
+        rate_area = 123
+        s = Single_Zip(zipcode, state, county_code, name, rate_area)
+
+        self.assertEquals(s.zipcode, zipcode)
+        self.assertEquals(s.state, state)
+        self.assertEquals(s.county_code, county_code)
+        self.assertEquals(s.name, name)
+        self.assertEquals(s.rate_area, rate_area)
+
+    def test_add(self):
+        zipcode = 12345
+        state = "AA"
+        county_code = "54321"
+        name = "County"
+        rate_area = 123
+
+        z = Zips()
+        z.add(zipcode, state, county_code, name, rate_area)
+
+        added_zipcode = z.zips[zipcode]
+
+        self.assertEquals(added_zipcode.zipcode, zipcode)
+        self.assertEquals(added_zipcode.state, state)
+        self.assertEquals(added_zipcode.county_code, county_code)
+        self.assertEquals(added_zipcode.name, name)
+        self.assertEquals(added_zipcode.rate_area, rate_area)
